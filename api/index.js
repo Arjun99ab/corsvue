f1098a3eeb028efdd54ee772458c548d1e6e2f60
Arjun99ab <53882059+Arjun99ab@users.noreply.github.com>
2024-04-08 13:47:10 -0400

    vercel calls

--- api/index.js
+++ api/index.js
@@ -1,42 +1,20 @@
-var express = require('express'),
-    request = require('request'),
-    bodyParser = require('body-parser'),
-    app = express();
+const axios = require('axios');
 
-var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
-console.log('Using limit: ', myLimit);
-
-app.use(bodyParser.json({limit: myLimit}));
-
-app.all('*', function (req, res, next) {
-
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-    res.header("Access-Control-Allow-Credentials", 'true');
-
-    if (req.method === 'OPTIONS') {
-        // CORS Preflight
-        res.send();
-    } else {
-        var targetURL = req.header('Target-URL'); // Target-URL ie. https://example.com or http://example.com
-        if (!targetURL) {
-            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
-            return;
-        }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
-            function (error, response, body) {
-                if (error) {
-                    console.error('error: ' + response.statusCode)
-                }
-//                console.log(body);
-            }).pipe(res);
+export default function handler(req, res, next) {
+  async function getURI(url) {
+    try {
+      const response = await axios.get(url);
+      if (response.status !== 200) {
+        return res.status(response.status).json({ type: 'error', message: response.statusText });
+      } else {
+      res.json(JSON.stringify(response.data))
+      }
+    } catch (error) {
+      console.log(error.message, "ERR")
+      // return res.status(500).json({ type: 'error', message: error.message });
     }
-});
-
-app.set('port', process.env.PORT || 3001);
-
-app.listen(app.get('port'), function () {
-    console.log('Proxy server listening on port ' + app.get('port'));
-});
+  } 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  getURI(JSON.parse(req.body)['my-url'])
+}
